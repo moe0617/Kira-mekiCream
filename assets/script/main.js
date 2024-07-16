c0c79bac3d9edc1cef157e651cf401d17b09d07a
moe0617 <lingmumengzhi@gmail.com>
2024-07-16 13:59:05 +0900

    2024/07/16 slick修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,31 @@
 // カルーセル
-$('.voices__slick').slick({
+$('.voices__slick__slide').slick({
     infinite: true,
     dots: true,
     arrows: true,
     prevArrow: '<img src="assets/image/voices/arrow-left-circle_sp.svg" class="slide-arrow prev-arrow">',
     nextArrow: '<img src="assets/image/voices/arrow-right-circle_sp.svg" class="slide-arrow next-arrow">',
 
-    centerMode: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    variableWidth: true,
     // autoplay: true,
     // autoplaySpeed: 7000,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            },
+
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
         },
-    ],
+    },
+    {
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        },
+    },
+    ]
 });
 
 // アコーディオン
