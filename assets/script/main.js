34c6402d8744b38f24a5672c7d3bd38ee0130bc4
moe0617 <lingmumengzhi@gmail.com>
2024-07-16 19:26:36 +0900

    2024/07/16 初回レビュー用

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,8 +8,8 @@ $('.voices__slick__slide').slick({
 
     slidesToShow: 3,
     slidesToScroll: 3,
-    // autoplay: true,
-    // autoplaySpeed: 7000,
+    autoplay: true,
+    autoplaySpeed: 7000,
 
     responsive: [{
         breakpoint: 1200,
@@ -34,4 +34,20 @@ $('.accordion__title').on('click', function(e) {
     var content = $(this).next();
     $(this).toggleClass('selected');
     content.slideToggle();
+});
+
+
+// ページトップに戻る
+var buttonToTop = $('#button-to-top');
+var position = 0;
+var speed = 1000;
+
+buttonToTop.on('click', function() {
+
+    // アニメーションをしながらページトップに移動
+    $("html, body").animate({
+        scrollTop: position
+    }, speed, "swing");
+    return false;
+
 });
